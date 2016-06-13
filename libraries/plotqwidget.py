7784895b950950fa9518461b17fc38161665589a
carmelom <carmelo.mordini@sns.it>
2016-06-14 00:58:24 +0200

    Keep this branck with the matplotlib-based gui

--- libraries/plotqwidget.py
+++ libraries/plotqwidget.py
@@ -4,47 +4,16 @@ Created on Sun May 24 01:46:57 2015
 
 @author: carmelo
 """
-import PyQt4
 from PyQt4 import QtCore, QtGui
 
 import numpy as np
-import pyqtgraph as pg
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gs
+from matplotlib.backends.backend_qt4agg import (
+    FigureCanvasQTAgg as FigureCanvas,
+    NavigationToolbar2QT as NavigationToolbar)
 
-from matplotlib.cm import get_cmap
 
-pg.setConfigOption('background', 'w')
-pg.setConfigOption('foreground', 'k')
-
-class RawSis():
-    
-    def __init__(self, filename, scale=None):
-        
-        im0, im1, im, raw = self.readsis(filename)
-        if scale is not None:
-            self.scale = scale
-        else:
-            self.scale = 2**16/10.0#1.0
-        
-        self.im0 = im0/self.scale
-        self.im1 = im1/self.scale
-        self.im_full  = im/self.scale
-        self.raw = raw/self.scale
-        
-    
-    def readsis(self,filename):
-        f = open(filename, 'rb')  #apre in binario
-        rawdata = np.fromfile(f,'H').astype(int)
-        f.close()
-        
-        width=rawdata[6]  # N cols
-        height=rawdata[5] # N rows
-        #rispetto ad octave, gli indici cambiano (python is 0-based)
-        image = rawdata[-width*height : ]
-        image.resize(height,width)
-        im0 = image[:height//2, :]
-        im1 = image[height//2:, :]
-        
-        return im0, im1, image, rawdata #, image.shape
 
 class PlotQWidget(QtGui.QWidget):
     
@@ -67,56 +36,52 @@ class PlotQWidget(QtGui.QWidget):
         self.plotLayout.addWidget(self.nameLabel)
         
 #        self.figure, self.ax = plt.subplots(1,1, figsize=(18,6))
-        self.imv = pg.ImageView()
-
-        self.plotLayout.addWidget(self.imv)
+        self.figure = plt.figure(figsize=(18,6))
+        self.gridspec = gs.GridSpec(2, 2, width_ratios=[1,2], height_ratios=[4,1])
+        self.ax = self.figure.add_subplot(self.gridspec[1])
+        
+        self.ax_y = self.figure.add_subplot(self.gridspec[0], sharey=self.ax)
+        self.ax_x = self.figure.add_subplot(self.gridspec[3], sharex=self.ax)
+        
+        self.figure.set_facecolor('none')
+        self.canvas = FigureCanvas(self.figure)
+        self.toolbar = NavigationToolbar(self.canvas, self, coordinates=False)
+        self.toolbar.setOrientation(QtCore.Qt.Horizontal)
+        self.plotLayout.addWidget(self.canvas)
+        self.plotLayout.addWidget(self.toolbar)
         
         self.setLayout(self.plotLayout)
+        self.canvas.draw()
         
-    def replot(self, image, dic=None, name=None, **kwargs):
+    def replot(self, image, dic={}, name=None):
         if name is not None:
             self.nameLabel.setText(name)
-            levels = (dic['vmin'], dic['vmax'])
-            cmap = get_cmap(dic['cmap'])
-            cmap_array = np.array([cmap(j) for j in range(cmap.N)])
-            cmap_pg = pg.ColorMap(np.arange(cmap.N)/cmap.N, cmap_array)
-            self.imv.setImage(image.T,)
-            self.imv.setLevels(*levels)
-            self.imv.getHistogramWidget().gradient.setColorMap(cmap_pg)
+        if self.isVisible():
+            if self.lims is None:
+#                print('lims is None')
+                self.ax.cla()              
+#                self.ax.axis('off')
+                self.im = self.ax.imshow(image, **dic)
+                self.lims = (self.ax.get_xlim(), self.ax.get_ylim())
+#                print(self.lims)
+            else:
+                self.lims = (self.ax.get_xlim(), self.ax.get_ylim())
+#                print(self.lims)
+#                self.ax.cla()              
+#                self.ax.axis('off')
+                self.im.set_data(image)
+                self.im.set_cmap(dic['cmap'])
+                self.im.norm.vmin = dic['vmin']
+                self.im.norm.vmax = dic['vmax']
+                self.ax.set_xlim(*self.lims[0])
+                self.ax.set_ylim(*self.lims[1])
+            int_x = image.sum(0)
+            int_y = image.sum(1)
+            self.ax_x.cla()
+            self.ax_y.cla()
+            self.ax_x.plot(np.arange(len(int_x)), int_x)
+            self.ax_y.plot(int_y, np.arange(len(int_y)))
+            self.canvas.draw()
 
                
-         
-               
-if __name__ == '__main__':
-    import sys
-    
-    app = QtGui.QApplication(sys.argv)
-    
-    file = '/home/carmelo/eos2/data/images/20160330-EoS - Bragg trials-0008.sis'
-    dic = {'vmin': 0, 'vmax': 2.0}
-
-    cmap = get_cmap('gist_stern')
-    cmap_array = np.array([cmap(j) for j in range(cmap.N)])
-    cmap_pg = pg.ColorMap(np.arange(cmap.N)/cmap.N, cmap_array)
-
-    image = RawSis(file).im0.T
-
-    win = QtGui.QMainWindow()
-    imv = pg.ImageView()
-
-    imv.setImage(image)
-    imv.setLevels(0.0, 2.0)
-    imv.getHistogramWidget().gradient.setColorMap(cmap_pg)
-#    imv.updateImage()
-#    imv = pg.image(image)
-#    print(imv)
-#    imv.ui.histogram.gradient.setColorMap(cmap_pg)
-    
-    win.setCentralWidget(imv)
-    win.setWindowTitle('pyqtgraph example: ImageViewColor')
-    win.show()  #raisethe IndexError
-    
-    status = app.exec_()
-
-    sys.exit(status)
-
+        
